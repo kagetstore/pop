04fffce0e559e61e843c88f24c7fad2da0f1eacf
kagetstore <87493595+kagetstore@users.noreply.github.com>
2021-12-09 14:54:26 +0700

    Update xinz.js

--- message/xinz.js
+++ message/xinz.js
@@ -365,7 +365,9 @@ module.exports = async(xinz, msg, blocked, baterai, _afk, welcome, left) => {
                 break
             case prefix+'help': case prefix+'menu':{
                 axios.get(`https://api-ramlan.herokuapp.com/api/ucapan?timeZone=Asia/Jakarta`)
-                .then(async(ucapan) => {                            
+                .then(async(ucapan) => {    
+                let son = fs.readFileSync('./media/music/sound2.mp3')
+                xinz.sendMessage(from, son, audio, {quoted: mek, mimetype: 'audio.mp4', ptt:true})                        
                 let sisalimit = getLimit(sender, limitCount, limit)
                 let sisaGlimit = cekGLimit(sender, gcount, glimit)
                 let cekvip = ms(_prem.getPremiumExpired(sender, premium) - Date.now())
@@ -2466,7 +2468,7 @@ _Silahkan tunggu file media sedang dikirim mungkin butuh beberapa menit_`
             case prefix+'sound10':{
                 if (isLimit(sender, isPremium, isOwner, limitCount, limit)) return reply (`Limit kamu sudah habis silahkan kirim ${prefix}limit untuk mengecek limit`)
                 let son = fs.readFileSync('./media/music/sound10.mp3')
-                xinz.sendMessage(from, son, audio, { mimetype: 'audio/mp4', quoted: mek, ptt: true })              
+                xinz.sendMessage(from, son, audio, {quoted: mek, mimetype: 'audio.mp4', ptt:true})              
                 limitAdd(sender, limit)
                 }
                 break
